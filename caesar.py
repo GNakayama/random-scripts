cc895856e27009a81e7733439bc909669de9d610
Guilherme <guilherme.nakayama.s@gmail.com>
2015-10-04 17:08:34 -0300

    Caesar Cipher

---
+++ caesar.py
@@ -0,0 +1,31 @@
+def encrypt(msg, key):
+	cipher = ""	
+
+	for letter in msg:
+		if letter in key:
+			cipher += key[letter]
+		else:
+			cipher += letter
+
+	return cipher
+
+def decrypt(cipher, key):
+	msg = ""
+
+	inverse_key = {v: k for k, v in map.items()}
+
+	for letter in cipher:
+		if letter in inverse_key:
+			msg += inverse_key[letter]
+		else:
+			msg += letter
+
+	return msg
+
+def gen_key(n):
+	key = {}
+
+	for i in xrange(26):
+		key[chr(65 + i)] = chr((i + n)%26)
+
+	return key
